09d654291e12aa694216a01f7db9257f8e8290aa
Skyler Fenn <skylerfenn@msn.com>
2016-12-21 23:47:45 -0700

    Initial Commit

---
+++ app/containers/App/index.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * App.react.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import React from 'react';
+import Helmet from 'react-helmet';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+function App(props) {
+  return (
+    <MuiThemeProvider>
+      <div>
+        <Helmet
+          titleTemplate="%s - Ruah Logistics"
+          defaultTitle="Team Ruah"
+          meta={[
+          { name: 'description', content: 'Team Ruah Product Management' },
+        ]}
+        />
+        <Header />
+        {React.Children.toArray(props.children)}
+        <Footer />
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+App.propTypes = {
+  children: React.PropTypes.node,
+};
+
+export default App;
---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,38 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React, { PureComponent } from 'react';
+import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+export class HomePage extends PureComponent {
+
+  render() {
+    return (
+      <article>
+        <Helmet
+          title="Home Page"
+          meta={[
+            { name: 'description', content: 'Ruah Home Page' },
+          ]}
+        />
+        App
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {};
+
+export function mapDispatchToProps() {
+  return {};
+}
+
+const mapStateToProps = createStructuredSelector({});
+
+// Wrap the component to inject dispatch and state into it
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
