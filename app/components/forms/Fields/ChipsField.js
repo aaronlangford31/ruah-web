d10c9ef3350cf7e92dd52e64e496f0badfaf83cf
Skyler Fenn <skylerfenn@msn.com>
2017-01-27 21:31:45 -0700

    Progress

--- app/components/forms/Fields/ChipsField.js
+++ app/components/forms/Fields/ChipsField.js
@@ -1,29 +1,26 @@
 import React, { PropTypes } from 'react';
 import ChipInput from 'material-ui-chip-input';
 
-const ChipsField = ({ input, label, hintText }) => {
-  console.log(input.value);
-  return (
-    <ChipInput
-      style={{ margin: '0 10px' }}
-      value={input.value || []}
-      onRequestAdd={(chip) => {
-        let values = input.value || [];
-        values = values.slice();
-        values.push(chip);
-        input.onChange(values);
-      }}
-      onRequestDelete={(chip) => {
-        let values = input.value || [];
-        values = values.filter((v) => v !== chip);
-        input.onChange(values);
-      }}
-      onBlur={() => input.onBlur()}
-      hintText={hintText}
-      floatingLabelText={label}
-    />
-  );
-}
+const ChipsField = ({ input, label, hintText }) => (
+  <ChipInput
+    style={{ margin: '0 10px' }}
+    value={input.value || []}
+    onRequestAdd={(chip) => {
+      let values = input.value || [];
+      values = values.slice();
+      values.push(chip);
+      input.onChange(values);
+    }}
+    onRequestDelete={(chip) => {
+      let values = input.value || [];
+      values = values.filter((v) => v !== chip);
+      input.onChange(values);
+    }}
+    onBlur={() => input.onBlur()}
+    hintText={hintText}
+    floatingLabelText={label}
+  />
+);
 
 ChipsField.propTypes = {
   input: PropTypes.object,
--- app/components/forms/Fields/index.js
+++ app/components/forms/Fields/index.js
@@ -1,10 +1,6 @@
 import BulletFields from './BulletFields';
 import CheckboxField from './CheckboxField';
-
-// import ChipsField from './ChipsField';
-
-import React, { PropTypes } from 'react';
-import ChipInput from 'material-ui-chip-input';
+import ChipsField from './ChipsField';
 import FileField from './FileField';
 import FileFields from './FileFields';
 import TextField from './TextField';
@@ -13,39 +9,9 @@ import TextFields from './TextFields';
 export {
   BulletFields,
   CheckboxField,
-
-  // ChipsField,
-
+  ChipsField,
   FileField,
   FileFields,
   TextField,
   TextFields,
-};
-
-export const ChipsField = ({ input, label, hintText }) => (
-  <ChipInput
-    style={{ margin: '0 10px' }}
-    {...input}
-    value={input.value || []}
-    onRequestAdd={(addedChip) => {
-      let values = input.value || [];
-      values = values.slice();
-      values.push(addedChip);
-      input.onChange(values);
-    }}
-    onRequestDelete={(deletedChip) => {
-      let values = input.value || [];
-      values = values.filter((v) => v !== deletedChip);
-      input.onChange(values);
-    }}
-    onBlur={() => input.onBlur()}
-    hintText={hintText}
-    floatingLabelText={label}
-  />
-);
-
-ChipsField.propTypes = {
-  input: PropTypes.object,
-  label: PropTypes.string,
-  hintText: PropTypes.string,
 };
