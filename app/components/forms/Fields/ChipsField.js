8b51f1e88cd49bcefc1033675c0e9c30c9922382
Skyler Fenn <skylerfenn@msn.com>
2017-01-28 17:23:29 -0700

    Fix Chips Fields

--- app/components/forms/Fields/ChipsField.js
+++ app/components/forms/Fields/ChipsField.js
@@ -1,20 +1,21 @@
 import React, { PropTypes } from 'react';
 import ChipInput from 'material-ui-chip-input';
+import { fromJS } from 'immutable';
 
 const ChipsField = ({ input, label, hintText }) => (
   <ChipInput
     style={{ margin: '0 10px' }}
-    value={input.value || []}
+    value={input.value.toJS() || []}
     onRequestAdd={(chip) => {
-      let values = input.value || [];
+      let values = input.value ? input.value.toJS() : [];
       values = values.slice();
       values.push(chip);
-      input.onChange(values);
+      input.onChange(fromJS(values));
     }}
     onRequestDelete={(chip) => {
-      let values = input.value || [];
+      let values = input.value ? input.value.toJS() : [];
       values = values.filter((v) => v !== chip);
-      input.onChange(values);
+      input.onChange(fromJS(values));
     }}
     onBlur={() => input.onBlur()}
     hintText={hintText}
