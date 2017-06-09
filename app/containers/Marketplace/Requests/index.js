73dbc82d05a64720b1946dc77e1c97dcda90901e
Aaron Langford <aaron.langford31@gmail.com>
2017-06-09 02:30:00 -0600

    Requests page first few lines

---
+++ app/containers/Marketplace/Requests/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import _ from 'underscore';
+
+class RequestsPage extends React.Component {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RequestsPage);
